0d501153481f9f8e5067edc374876d799321b27b
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-23 17:16:27 -0300

    added styles for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,19 +3,22 @@ import Button from './atoms/Button'
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 export const Contact = () => {
   return (
-    <div>
+    <section id='contact' className='contact'>
       <h2>contact</h2>
       <p>feel free to contact me below if you have any questions, or email me at chichicuellodev@gmail.com</p>
-      <form>
+      <form className='form'>
+        <div>
         <label htmlFor="name"> name
         </label>
         <input type="text" name="name" id="name" />
-        <label htmlFor="email"> email
+        </div>
+<div>        <label htmlFor="email"> email
         </label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="message"> leave me a message
+        <input type="email" name="email" id="email" /></div>
+<div>        <label htmlFor="message"> leave me a message
         </label>
-        <textarea name="message" id="message"></textarea>
+        <textarea name="message" id="message"></textarea></div>
+
         <button type="submit">SEND</button>
       </form>
       <div>
@@ -26,6 +29,6 @@ export const Contact = () => {
         </ul>
       </div>
       <Button text="DOWNLOAD CV"/>
-    </div>
+    </section>
   )
 }
