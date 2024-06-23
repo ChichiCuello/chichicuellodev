9fece93e36217746363aeb09f25d5bd07762940f
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-23 13:31:31 -0300

    working on bento box

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,49 +11,78 @@ import react from '../assets/images/react.png'
 import tailwind from '../assets/images/tailwind.png'
 
 export const Skills = () => {
-  return (
-    <div>
-      <h2>skills</h2>
-      <div>
-        <div>
-          <div>
-            <ul>
-              <li><img src={git} alt="" /></li>
-              <li><img src={github} alt="" /></li>
-            </ul>
-          </div>
-          <h3>version control</h3>
-        </div>
-        <div>
-          <h3>ux/ui</h3>
-          <div>
-            <img src={figma} alt="" />
-          </div>
-        </div>
-        <div>
-          <div>
-            <h3>project managment apps</h3>
-            <ul>
-              <li><img src={jira} alt="" /></li>
-              <li><img src={trello} alt="" /></li>
-            </ul>
-          </div>
-          <div>
-            <div>
-              <ul>
-                <li><img src={html} alt="" /></li>
-                <li><img src={css} alt="" /></li>
-                <li><img src={js} alt="" /></li>
-              </ul>
-              <ul>
-                <li><img src={react} alt="" /></li>
-                <li><img src={tailwind} alt="" /></li>
-              </ul>
-              <div><h3>frontend</h3></div>
-            </div>
-          </div>
-        </div>
-      </div>
+  return (<>
+  <div class="parent">
+    <div class="div1">
+    <ul>
+        <li><img src={git} alt="" /></li>
+        <li><img src={github} alt="" /></li>
+      </ul>
     </div>
+    <div class="div2"><h3>version control</h3> </div>
+    <div class="div3"><h3>ux/ui</h3> </div>
+    <div class="div4"><img src={figma} alt="" /></div>
+    <div class="div5"><h3>project managment apps</h3> </div>
+    <div class="div6"><ul>
+        <li><img src={jira} alt="" /></li>
+        <li><img src={trello} alt="" /></li>
+      </ul></div>
+    <div class="div7">        <ul>
+          <li><img src={html} alt="" /></li>
+          <li><img src={css} alt="" /></li>
+          <li><img src={js} alt="" /></li>
+        </ul> </div>
+      <div class="div8"><h3>frontend</h3> </div>
+    <div class="div9">         <ul>
+          <li><img src={react} alt="" /></li>
+          <li><img src={tailwind} alt="" /></li>
+        </ul></div>
+</div>
+  </>
+
   )
 }
+
+{/* <div>
+<h2>skills</h2>
+<div>
+  <div>
+    <div>
+      <ul>
+        <li><img src={git} alt="" /></li>
+        <li><img src={github} alt="" /></li>
+      </ul>
+    </div>
+    <h3>version control</h3>
+  </div>
+  <div>
+    <h3>ux/ui</h3>
+    <div>
+      <img src={figma} alt="" />
+    </div>
+  </div>
+  <div>
+    <div>
+      <h3>project managment apps</h3>
+      <ul>
+        <li><img src={jira} alt="" /></li>
+        <li><img src={trello} alt="" /></li>
+      </ul>
+    </div>
+    <div>
+      <div>
+        <ul>
+          <li><img src={html} alt="" /></li>
+          <li><img src={css} alt="" /></li>
+          <li><img src={js} alt="" /></li>
+        </ul>
+        <ul>
+          <li><img src={react} alt="" /></li>
+          <li><img src={tailwind} alt="" /></li>
+        </ul>
+        <div><h3>frontend</h3></div>
+      </div>
+    </div>
+  </div>
+</div>
+</div> */}
