ceaa7b4754730fa6f9fdd20a0d3223bdc37a5005
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-24 10:47:18 -0300

    Footer styles desktop and mobile

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,7 +11,7 @@ import react from '../assets/images/react.png'
 import tailwind from '../assets/images/tailwind.png'
 
 export const Skills = () => {
-  return (<>
+  return (<section className='skills' id='skills'>
   <h2>skills</h2>
   <div class="parent">
     <div class="div1 tech-stack">
@@ -38,51 +38,7 @@ export const Skills = () => {
           <div><img src={tailwind} alt="" /></div>
         </div>
 </div>
-  </>
+  </section>
 
   )
 }
-
-{/* <div>
-<h2>skills</h2>
-<div>
-  <div>
-    <div>
-      <ul>
-        <li><img src={git} alt="" /></li>
-        <li><img src={github} alt="" /></li>
-      </ul>
-    </div>
-    <h4>version control</h4>
-  </div>
-  <div>
-    <h3>ux/ui</h3>
-    <div>
-      <img src={figma} alt="" />
-    </div>
-  </div>
-  <div>
-    <div>
-      <h3>project managment apps</h3>
-      <ul>
-        <li><img src={jira} alt="" /></li>
-        <li><img src={trello} alt="" /></li>
-      </ul>
-    </div>
-    <div>
-      <div>
-        <ul>
-          <li><img src={html} alt="" /></li>
-          <li><img src={css} alt="" /></li>
-          <li><img src={js} alt="" /></li>
-        </ul>
-        <ul>
-          <li><img src={react} alt="" /></li>
-          <li><img src={tailwind} alt="" /></li>
-        </ul>
-        <div><h3>frontend</h3></div>
-      </div>
-    </div>
-  </div>
-</div>
-</div> */}
